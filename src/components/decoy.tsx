dba330c43da39119bdce5ac0395e41038c059d9a
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-10-10 15:29:07 +0530

    Update decoy.tsx

--- src/components/decoy.tsx
+++ src/components/decoy.tsx
@@ -3,16 +3,20 @@ import bg2 from "../assets/bg2.webp";
 import c2 from "../assets/abstract/2.webp";
 import c3 from "../assets/abstract/3.webp";
 import m13d from "../assets/space/1_3D.png";
+import giphy from "../assets/giphy.webp";
+import wall from "../assets/wall3.jpg";
 
 
 const Decoy = () => {
   return (
     <div>
         <h1 className="font-paytone">hii</h1>
-        <img src={bg1} className="w-1 h1" />
-        <img src={bg2} className="w-1 h1" />
+        <img src={giphy} className="w-1 h1" />
+        <img src={wall} className="w-1 h1" />
         <img src={c2} className="w-1 h1" />
         <img src={c3} className="w-1 h1" />
+        <img src={bg1} className="w-1 h1" />
+        <img src={bg2} className="w-1 h1" />
         <img src={m13d} className="w-1 h1" />
     </div>
   )
