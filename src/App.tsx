791b8c85b30156bd0ac7038879bf075e86993fbd
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-10-10 15:09:33 +0530

    Added Decoy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import { GlobalState } from "./context/GlobalContext";
 import Home from "./pages/home";
+import Loading from "./components/loading";
+import Decoy from "./components/decoy";
 
 function App() {
 
-  
-
+  const [loading, setLoading] = useState(true);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false);
+      window.scrollTo(0, 0)
+    },2500)
+  }, [])
 
   return (
+    loading?<>
+    <Loading />
+    <Decoy />
+    </>:
     <div className="text-white">
       <GlobalState>
         <Home />
---
+++ src/components/decoy.tsx
@@ -0,0 +1,21 @@
+import bg1 from "../assets/bg.webp";
+import bg2 from "../assets/bg2.webp";
+import c2 from "../assets/abstract/2.webp";
+import c3 from "../assets/abstract/3.webp";
+import m13d from "../assets/space/1_3D.png";
+
+
+const Decoy = () => {
+  return (
+    <div>
+        <h1 className="font-paytone">hii</h1>
+        <img src={bg1} className="w-1 h1" />
+        <img src={bg2} className="w-1 h1" />
+        <img src={c2} className="w-1 h1" />
+        <img src={c3} className="w-1 h1" />
+        <img src={m13d} className="w-1 h1" />
+    </div>
+  )
+}
+
+export default Decoy
